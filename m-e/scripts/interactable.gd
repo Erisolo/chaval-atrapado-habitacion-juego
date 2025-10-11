be94b9311250a11c79273eba0521dd703154678d
Erisolo <marsolor@ucm.es>
2025-10-11 20:33:24 +0200

    habitación a medio montar y hamster empezado

--- m-e/scripts/interactable.gd
+++ m-e/scripts/interactable.gd
@@ -1,16 +1,20 @@
 extends RigidBody2D
-@onready var animated_sprite_2d: AnimatedSprite2D = $Area2D/AnimatedSprite2D
+@export var collison_area: Area2D
 var isIn = false
-const TEST_DIALOGUE = preload("uid://cktesfjwl7ytt")
+@export var dialogue : DialogueResource
+var dialogueStartPoint = "start"
 var dialogueActive = false
 
 func _ready() -> void:
 	DialogueManager.dialogue_started.connect(dialogueOn)
 	DialogueManager.dialogue_ended.connect(dialogueOff)
+	collison_area.body_entered.connect(_on_area_2d_body_entered)
+	collison_area.body_exited.connect(_on_area_2d_body_exited)
 	
 func _input(event: InputEvent) -> void:
 	if isIn && event.is_action_pressed("Interact") && not dialogueActive:
-		DialogueManager	.show_dialogue_balloon(TEST_DIALOGUE, "start")
+		DialogueManager	.show_dialogue_balloon(dialogue, dialogueStartPoint)
+		
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	isIn = true
