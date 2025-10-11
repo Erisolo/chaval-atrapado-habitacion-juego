5d01b5cdb025eaf2bcb4595bb53aa02e754e2fa3
Erisolo <marsolor@ucm.es>
2025-10-11 13:09:08 +0200

    hay dialogos etc pero mal

--- m-e/scripts/interactable.gd
+++ m-e/scripts/interactable.gd
@@ -1,13 +1,25 @@
 extends RigidBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $Area2D/AnimatedSprite2D
 var isIn = false
+const TEST_DIALOGUE = preload("uid://cktesfjwl7ytt")
+var dialogueActive = false
 
+func _ready() -> void:
+	DialogueManager.dialogue_started.connect(dialogueOn)
+	DialogueManager.dialogue_ended.connect(dialogueOff)
+	
 func _input(event: InputEvent) -> void:
-	if isIn && event.is_action_pressed("Interact"):
-		print("Interacted")
+	if isIn && event.is_action_pressed("Interact") && not dialogueActive:
+		DialogueManager	.show_dialogue_balloon(TEST_DIALOGUE, "start")
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	isIn = true
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	isIn = false
+
+func dialogueOn(dialogue) -> void:
+	dialogueActive = true
+
+func dialogueOff(dialogue)-> void:
+	dialogueActive = false
