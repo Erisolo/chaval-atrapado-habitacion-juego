8d48a04a4a146be40ac0373029f5e16398efa647
Erisolo <marsolor@ucm.es>
2025-10-12 16:31:15 +0200

    mergeo etc

---
+++ m-e/scripts/checkNoteAnimationController.gd
@@ -0,0 +1,22 @@
+extends CanvasLayer
+signal button_pressed;
+@onready var checknote = $TextureRect
+@onready var animationplayer = $AnimationPlayer
+
+func _ready() -> void:
+	checknote.visible = false;
+	
+func _process(_delta):
+	if checknote.visible && Input.is_action_just_pressed("ui_accept"):
+		button_pressed.emit()
+		
+#es un poko guarrada pero me la pela
+func showChores():
+	checknote.visible = true
+	animationplayer.play("jump_in")
+	await animationplayer.animation_finished
+	await button_pressed
+	animationplayer.play("jump_out")
+	await animationplayer.animation_finished
+	checknote.visible = false;
+	
