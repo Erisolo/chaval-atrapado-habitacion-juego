df1378b5858458f82ca8a29fc7fb4308ce74f3ca
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2025-10-12 17:39:13 +0200

    pitocorto

--- m-e/scripts/ChoreManager.gd
+++ m-e/scripts/ChoreManager.gd
@@ -5,6 +5,9 @@ var initialDialogue = preload("res://Dialogues/WakeUp.dialogue")
 # Hamster route assets
 var hamsterFood = preload("res://prefabs/hamster_food.tscn")
 var hamsterDialogue = preload("res://Dialogues/HamsterDilogues.dialogue")
+# InsideYou route assets
+var stomachAcheArea = preload("res://prefabs/stomach_ache.tscn")
+var insideYouDialogue = preload("res://Dialogues/InsideYouDialogues.dialogue")
 
 func _ready() -> void:
 	Checklist.ChoreAdded.connect(_add_chore_listener)
@@ -20,7 +23,14 @@ func _add_chore_listener():
 	var chore = Checklist.chores[Checklist.numChores - 1]
 	chore.stepCompleted.connect(_manage_step)
 	_manage_step(chore)
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("Hamster3"):
+		var chore = Checklist.getChore("Hamster")
+		chore.currentStep = 3
+		_manage_step(chore)
 
+# Siento mucho lo que voy a hacer aquí
 func _manage_step(chore: Chore):
 	if chore.name == "Hamster":
 		var hamster = $hamster
@@ -37,3 +47,11 @@ func _manage_step(chore: Chore):
 		elif chore.currentStep == 3:
 			hamster.dialogueStartPoint = "NoHamster"
 			DialogueManager.show_dialogue_balloon(hamsterDialogue, "EatYourHamster")
+			if(Checklist.getChore("InsideYou") == null):
+				var area = stomachAcheArea.instantiate()
+				area.global_position = Vector2(330, 150)
+				add_child(area)
+	elif chore.name == "InsideYou":
+		if chore.currentStep == 0:
+			DialogueManager.show_dialogue_balloon(insideYouDialogue)
+			# Hacer que los dientes sean pastillas
---
+++ m-e/scripts/stomach_ache.gd
@@ -0,0 +1,9 @@
+extends Area2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	body_entered.connect(_start_stomach_ache)
+
+func _start_stomach_ache(body: Node2D) -> void:
+	Checklist.addChore("InsideYou", ["Take pills", "Check yourself in the mirror", "Use the scissors", ""])
+	get_parent().remove_child(self)
