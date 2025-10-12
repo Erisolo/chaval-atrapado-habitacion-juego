5860d39ff55fc5e9ade5b37f2719683708352781
Erisolo <marsolor@ucm.es>
2025-10-12 18:27:40 +0200

    CHORE NOTEBOOK FUNCIONAL

--- m-e/scripts/PlayerMovement.gd
+++ m-e/scripts/PlayerMovement.gd
@@ -15,7 +15,6 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_vector("Left", "Right", "Up", "Down")
-	print(animated_sprite.animation)
 	if !dialogueActive:
 		if direction.y > 0:
 			animated_sprite.play("walk-front")
