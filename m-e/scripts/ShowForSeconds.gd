98a8e0f01467e4b2d268d494e696ba85cc3b5dc5
Erisolo <marsolor@ucm.es>
2025-10-14 16:08:30 +0200

    haciendo el espejo más fantabuloso

--- m-e/scripts/ShowForSeconds.gd
+++ m-e/scripts/ShowForSeconds.gd
@@ -1,11 +1,17 @@
 extends Sprite2D
 
-@export var seconds: int
 @export var Scene: String
 @export var chore: String
+@export var animator: AnimatedSprite2D
+signal button_pressed;
 
+func _process(_delta) -> void:
+	if Input.is_action_just_pressed("ui_accept"):
+		button_pressed.emit()
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	await get_tree().create_timer(seconds).timeout
+	await self.button_pressed
+	animator.play("eye")
+	await self.button_pressed
 	Checklist.getChore(chore).finishCurrentStep()
 	SceneManager.changeScene(Scene)
