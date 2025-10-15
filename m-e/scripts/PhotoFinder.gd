631c4d28f8ad36f6dca09ab3368aecdb305ca2f3
Erisolo <marsolor@ucm.es>
2025-10-16 01:09:23 +0200

    even more sounds (I think this is it)

--- m-e/scripts/PhotoFinder.gd
+++ m-e/scripts/PhotoFinder.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 signal changeScene
+const BOOM = preload("uid://ddntidanqd3jv")
 
 @export var objectives: Array[Node2D]
 var nObjectives: int
@@ -13,6 +14,7 @@ func _ready() -> void:
 
 func _on_objective_clicked():
 	nObjectives -= 1
+	SoundManager.play_sound(BOOM)
 	
 	if nObjectives <= 0:
 		Checklist.getChore("Photos").finishCurrentStep()
