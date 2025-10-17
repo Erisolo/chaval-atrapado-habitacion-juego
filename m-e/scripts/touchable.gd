89d6001a05f835391072640195280084521ac468
Mika Rodríguez Castro <149060113+MikalMica@users.noreply.github.com>
2025-10-17 08:53:49 +0200

    bugs varios

--- m-e/scripts/touchable.gd
+++ m-e/scripts/touchable.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
 signal wasClicked
+var doneOnce = false
 
 func _ready() -> void:
 	input_event.connect(on_input_event)
@@ -8,4 +9,6 @@ func _ready() -> void:
 
 func on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
 	if event.is_action_pressed("LeftClick"):
-		wasClicked.emit()
+		if !doneOnce:
+			wasClicked.emit()
+			doneOnce = true
