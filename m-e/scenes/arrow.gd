8640a1d2943fb24cd53e8c2d898a05f4db51f30d
Erisolo <marsolor@ucm.es>
2026-08-14 11:42:50 +0200

    adding clue to photo minigame

--- m-e/scenes/arrow.gd
+++ m-e/scenes/arrow.gd
@@ -1,12 +1,15 @@
 extends Sprite2D
 
 @export var timeToWait = 10
+@export var action: String
 var timer = 0.0
 
 func _process(delta: float) -> void:
 	timer += delta
-	if Input.is_action_pressed("Up"):
+	if Input.is_action_pressed(action):
 		timer = 0.0
+		visible = false
+		
 	
 	if timer >= timeToWait:
 		if timer  >= 0.5 + timeToWait:
